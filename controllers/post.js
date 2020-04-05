e0bdb49416ca197525104c8d1e36a24b2ac99176
You Song Hou <yousonghou@yahoo.com>
2020-04-04 22:05:48 -0400

    add a route to get all posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,21 @@
 const Post = require("../models/Post");
 
+// GET /post
+exports.findAllPost = async(req, res, next) => {
+    try{
+        const posts = await Post.find();
+
+        return res.status(200).json({
+            msg: "All posts found",
+            data: posts,
+            count: posts.length
+        })
+    }catch(err){
+        console.log("error in GET /post" + err);
+        res.status(500).json({ error: "Something went wrong in the server"});
+    }
+};
+
 // POST /post
 exports.createPost = async(req, res, next) => {
     try{
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,7 @@ const express = require("express");
 const postController = require("../controllers/post");
 const router = express.Router();
 
+router.get('/', postController.findAllPost);
 router.post('/', postController.createPost);
 
 module.exports = router;
